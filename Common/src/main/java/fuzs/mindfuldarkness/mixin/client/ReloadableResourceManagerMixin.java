87f3496f47df3d368282e52f93873eea1090e73b
Fuzss <mrfusseel@gmail.com>
2023-04-20 09:57:44 +0200

    fix production

--- Common/src/main/java/fuzs/mindfuldarkness/mixin/client/ReloadableResourceManagerMixin.java
+++ Common/src/main/java/fuzs/mindfuldarkness/mixin/client/ReloadableResourceManagerMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.server.packs.resources.ReloadInstance;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraft.util.Unit;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -17,6 +18,7 @@ import java.util.concurrent.Executor;
 
 @Mixin(ReloadableResourceManager.class)
 abstract class ReloadableResourceManagerMixin {
+    @Shadow
     private CloseableResourceManager resources;
 
     @Inject(method = "createReload", at = @At(value = "FIELD", target = "Lnet/minecraft/server/packs/resources/ReloadableResourceManager;resources:Lnet/minecraft/server/packs/resources/CloseableResourceManager;", ordinal = 2))
